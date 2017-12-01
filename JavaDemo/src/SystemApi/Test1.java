f55fa07bd004f8e1b394b4a0d1a4fc80e2cfe61b
hongye <125299431>
2017-12-01 10:18:29 +0800

    java

--- JavaDemo/src/SystemApi/Test1.java
+++ JavaDemo/src/SystemApi/Test1.java
@@ -1,11 +1,14 @@
 package SystemApi;
 
+import java.util.Random;
+
 public class Test1 {
 	public static void main(String[] args) {
 //		System.out.println(Integer.parseInt("30"));
 //		test1();
 //		test2();
-		test3();
+//		test3();
+		test4();
 	}
 	
 	static void test1() {
@@ -53,7 +56,18 @@ public class Test1 {
 	}
 	
 	static void test4() {
-		
+		/*
+		 * 	练习三：循环生成100-999之间的的三位数并进行打印该数，当该数能被10整除时，结束运行的程序
+		 */
+		Random random = new Random();
+		while (true) {
+			int number = random.nextInt(900) + 100;
+			System.out.println(number);
+			if (number % 100 == 0) {
+				System.exit(0);
+			}
+			
+		}
 	}
 	
 	
